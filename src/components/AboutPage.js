2b67fd1beb73d4c47682936b2e0e93e967bd422a
Allen Jmnz <17911043+allenjmnz@users.noreply.github.com>
2020-04-15 22:11:28 -0500

    Initial Components set up and API calls

---
+++ src/components/AboutPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class AboutPage extends Component {
+  render() {
+    return (
+      <>
+        <h2>About</h2>
+        <p>This app uses React.</p>
+      </>
+    );
+  }
+}
+
+export default AboutPage;
---
+++ src/components/CoursesPage.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import { getCourses } from '../api/courseApi';
+
+const CoursesPage = () => {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    getCourses().then((_courses) => setCourses(_courses));
+  }, []);
+
+  return (
+    <>
+      <h2>Courses</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {courses.map((course) => (
+            <tr key={course.id}>
+              <td>{course.title}</td>
+              <td>{course.authorId}</td>
+              <td>{course.category}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default CoursesPage;
